c5cdc347c7a7c68f1676e08f62d163e530f42ac6
Ricardo <matosricardordg@gmail.com>
2025-04-11 02:17:53 -0300

    The first images of the game

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,26 +1,42 @@
+local love = require("love")
+
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
-function love.load()
-  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
-    fullscreen = false,
-    resizable = false,
-    vsync = true
-  })
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
-  squareX = 0
+function love.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  smallFont = love.graphics.newFont('font.ttf', 8)
+
+  love.graphics.setFont(smallFont)
+
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+                  fullscreen = false,
+                  resizable = false,
+                  vsync = true})
 end
 
-function love.update(dt)
-  if love.keyboard.isDown('a') then
-    squareX = squareX - 10
-  elseif love.keyboard.isDown('d') then
-    squareX = squareX + 10
+function love.keypressed(key)
+  if key == 'escape' then
+    love.event.quit()
   end
 end
 
 function love.draw()
-  love.graphics.printf("Hello world", 0, WINDOW_HEIGHT / 2 - 6, WINDOW_WIDTH, 'center')
-  
-  love.graphics.rectangle("fill", squareX, WINDOW_HEIGHT - 60, 50, 50)
+  push:apply('start')
+
+  love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+  love.graphics.printf("Hello world", 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+  love.graphics.rectangle('fill', 20, 10, 20, 5)
+  love.graphics.rectangle('fill', VIRTUAL_WIDTH - 40, VIRTUAL_HEIGHT - 20, 20, 5)
+
+  push:apply('end')
+
 end
